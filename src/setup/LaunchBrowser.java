5c3df6ab24c10f61c2523a5bb7ff74c94e5de16a
mayureshudas <mayuresh.udas@capgemini.com>
2017-08-08 14:05:14 +0530

    added files
    added files

---
+++ src/setup/LaunchBrowser.java
@@ -0,0 +1,49 @@
+package setup;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+
+public class LaunchBrowser {
+public static WebDriver driver;
+	@BeforeSuite
+@Parameters({"browser","url"})
+public static void launchBrowser(@Optional("chrome")String browser, String url){
+	/*if(browser=="firefox"){
+		System.out.println("Inside firefox box");
+		driver=new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get("url");
+		return driver;
+	}*/
+	
+	
+		System.out.println("Inside chrome box");
+		System.setProperty("webdriver.chrome.driver", "D:\\Users\\maudas\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		driver=new ChromeDriver();
+		//System.setProperty("webdriver.gecko.driver", "D:\\Users\\maudas\\Downloads\\geckodriver-v0.18.0-win64\\geckodriver.exe");
+		//DesiredCapabilities capabilities=DesiredCapabilities.firefox();
+		//capabilities.setCapability("marionette", true);
+		//WebDriver driver = new FirefoxDriver(capabilities);
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get("url");
+		
+		
+	
+	
+	
+		
+}
+	
+	public static WebDriver returnDriver(){
+		return driver;
+	}
+}
---
+++ src/test/Test1.java
@@ -0,0 +1,18 @@
+package test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+import setup.LaunchBrowser;
+
+public class Test1 {
+	public static WebDriver driver=LaunchBrowser.returnDriver();
+	@Test
+	public static void login(){
+		driver.findElement(By.xpath("//form//input[@id='UserName']")).sendKeys("admin123");
+		driver.findElement(By.xpath("//form//input[@id='Password']")).sendKeys("admin123");
+		driver.findElement(By.xpath("//form//input[@class='btn btn-default']")).click();
+	}
+
+}
