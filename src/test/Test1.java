1c915fb0b1952c0d9c30a6c63d0010990610e3f4
mayureshudas <mayuresh.udas@capgemini.com>
2017-08-08 15:28:29 +0530

    done changes

--- src/test/Test1.java
+++ src/test/Test1.java
@@ -5,11 +5,24 @@ import junit.framework.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
-
+import org.testng.annotations.BeforeSuite;
+import org.openqa.selenium.chrome.ChromeDriver;
 import setup.LaunchBrowser;
 
 public class Test1 {
-	public static WebDriver driver=LaunchBrowser.returnDriver();
+	public static WebDriver driver;
+	@BeforeSuite
+	public static void launchDriver(){
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Administrator\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		driver=new ChromeDriver();
+		//System.setProperty("webdriver.gecko.driver", "D:\\Users\\maudas\\Downloads\\geckodriver-v0.18.0-win64\\geckodriver.exe");
+		//DesiredCapabilities capabilities=DesiredCapabilities.firefox();
+		//capabilities.setCapability("marionette", true);
+		//WebDriver driver = new FirefoxDriver(capabilities);
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get("http://localhost/demotesting/account/login");
+	}
 	@Test
 	public static void login(){
 		driver.findElement(By.xpath("//form//input[@id='UserName']")).sendKeys("admin123");
@@ -20,7 +33,7 @@ public class Test1 {
 	@Test
 	public static void getHeader(){
 		String title=driver.getTitle();
-		Assert.assertEquals("", title);
+		Assert.assertEquals("An ASP .net website", title);
 		
 	}
 	
